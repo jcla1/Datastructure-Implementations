c41ce3a400e4ccb4bec0ecb0dbf45e37a9e21e7e
Joseph Adams <whitegolem@gmail.com>
2014-07-11 13:19:56 +0200

    Hash func capability in bloom filter

--- BloomFilter/bloom_filter.c
+++ BloomFilter/bloom_filter.c
@@ -1,13 +1,21 @@
 #include <stdlib.h>
+#include <stdarg.h>
 #include "bloom_filter.h"
 
-bloom_filter *bloom_filter_create(int num_bits) {
+bloom_filter *bloom_filter_create(int num_bits, int num_hash_fn, ...) {
+    va_list funcs;
+    bloom_filter *bf;
     // We only allow a number of bits that are
     // a multiple of 8 (because char == 8 bits)
-    if(num_bits & 7 != 0)
+    if((num_bits & 7) != 0)
         return NULL;
 
-    bloom_filter *bf = malloc(sizeof(bloom_filter));
+    // The bloom filter is of no use, when there
+    // are no hash functions.
+    if(num_hash_fn < 1)
+        return NULL;
+
+    bf = malloc(sizeof(bloom_filter));
     if(bf == NULL)
         return NULL;
 
@@ -17,12 +25,27 @@ bloom_filter *bloom_filter_create(int num_bits) {
         return NULL;
     }
 
+    bf->hash_funcs = malloc(sizeof(hash_fn) * num_hash_fn);
+    if(bf->hash_funcs == NULL) {
+        free(bf->bits);
+        free(bf);
+        return NULL;
+    }
+
+    va_start(funcs, num_hash_fn);
+    for(int i = 0; i < num_hash_fn; i++) {
+        bf->hash_funcs[i] = va_arg(funcs, hash_fn);
+    }
+    va_end(funcs);
+
     bf->num_bits = num_bits;
+    bf->num_hash_fn = num_hash_fn;
 
     return bf;
 }
 
 void bloom_filter_destroy(bloom_filter *bf) {
     free(bf->bits);
+    free(bf->hash_funcs);
     free(bf);
 }
--- BloomFilter/bloom_filter.h
+++ BloomFilter/bloom_filter.h
@@ -1,9 +1,16 @@
 #ifndef DATASTRUCTURES_BLOOM_FILTER_H_
 #define DATASTRUCTURES_BLOOM_FILTER_H_
 
+typedef int (*hash_fn)(void *, size_t);
+
 typedef struct bloom_filter {
     int num_bits;
+    int num_hash_fn;
+    hash_fn *hash_funcs;
     unsigned char *bits;
 } bloom_filter;
+
+bloom_filter *bloom_filter_create(int num_bits, int num_hash_fn, ...);
+void bloom_filter_destroy(bloom_filter *bf);
 
 #endif // DATASTRUCTURES_BLOOM_FILTER_H_

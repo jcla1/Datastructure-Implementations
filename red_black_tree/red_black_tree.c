6a6d931e8971e6fc0d4aa567e113280e65bde2d5
Joseph Adams <whitegolem@gmail.com>
2014-07-13 23:06:33 +0200

    Added search to RB-Tree

--- red_black_tree/red_black_tree.c
+++ red_black_tree/red_black_tree.c
@@ -4,3 +4,21 @@
 
 rb_tree *rb_tree_create(rb_cmp_fn cmp) {}
 void rb_tree_destroy(rb_tree *tree) {}
+void *rb_tree_search(rb_tree *tree, void *val) {
+  rb_tree_node *cur;
+  int comp_res;
+
+  cur = tree->root;
+  while(cur != NULL) {
+    comp_res = tree->cmp(cur->value, val);
+
+    if(comp_res == 0)
+      return cur->value;
+    else if(comp_res < 0)
+      cur = cur->left;
+    else
+      cur = cur->right;
+  }
+
+  return NULL; // value not present
+}
--- red_black_tree/red_black_tree.h
+++ red_black_tree/red_black_tree.h
@@ -1,22 +1,25 @@
 #ifndef DATASTRUCTURES_RED_BLACK_TREE_H_
 #define DATASTRUCTURES_RED_BLACK_TREE_H_
 
+// Will be passed the RB-Tree node first then a value
+// that might need to be extracted.
 typedef int (*rb_cmp_fn)(void *, void *);
 
 typedef struct rb_tree_node rb_tree_node;
 struct rb_tree_node {
-    void *value;
-    enum { RED, BLACK } color;
-    rb_tree_node *left, *right, *parent;
+  void *value;
+  enum { RED, BLACK } color;
+  rb_tree_node *left, *right, *parent;
 };
 
 typedef struct rb_tree rb_tree;
 struct rb_tree {
-    rb_cmp_fn cmp;
-    rb_tree_node *root;
+  rb_cmp_fn cmp;
+  rb_tree_node *root;
 };
 
 rb_tree *rb_tree_create(rb_cmp_fn cmp);
 void rb_tree_destroy(rb_tree *tree);
+void *rb_tree_search(rb_tree *tree, void *val);
 
 #endif // DATASTRUCTURES_RED_BLACK_TREE_H_

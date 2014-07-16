76fc6355f58a5ccaec9c2134f6b692627ecabace
Joseph Adams <whitegolem@gmail.com>
2014-07-16 21:34:51 +0100

    Added first functions

--- splay_tree/splay_tree.c
+++ splay_tree/splay_tree.c
@@ -1,3 +1,13 @@
 #include <stdlib.h>
 
 #include "splay_tree.h"
+
+splay_tree *splay_tree_create(splay_cmp_fn cmp) {}
+
+void splay_tree_destroy(splay_tree *tree) {}
+
+void *splay_tree_search(splay_tree *tree, void *value) {}
+
+void splay_tree_insert(splay_tree *tree, void *value) {}
+
+static void splay(splay_node *node) {}
--- splay_tree/splay_tree.h
+++ splay_tree/splay_tree.h
@@ -3,19 +3,24 @@
 
 typedef int (*splay_cmp_fn)(void *, void *);
 
-typedef struct splay_tree_node splay_tree_node;
-struct splay_tree_node {
-    splay_tree_node *left, *right, *parent;
+typedef struct splay_node splay_node;
+struct splay_node {
+    splay_node *left, *right, *parent;
     void *value;
 };
 
 typedef struct splay_tree splay_tree;
 struct splay_tree {
     splay_cmp_fn cmp_fn;
-    splay_tree_node *root;
+    splay_node *root;
 };
 
 splay_tree *splay_tree_create(splay_cmp_fn cmp);
 void splay_tree_destroy(splay_tree *tree);
+
+void *splay_tree_search(splay_tree *tree, void *value);
+void splay_tree_insert(splay_tree *tree, void *value);
+
+static void splay(splay_node *node);
 
 #endif // DATASTRUCTURES_SPLAY_TREE_H_

89d76432f56d7247c2d960ca6ab7a74676670325
Joseph Adams <whitegolem@gmail.com>
2014-07-16 21:36:09 +0100

    Added create function for splay tree

--- splay_tree/splay_tree.c
+++ splay_tree/splay_tree.c
@@ -2,7 +2,17 @@
 
 #include "splay_tree.h"
 
-splay_tree *splay_tree_create(splay_cmp_fn cmp) {}
+splay_tree *splay_tree_create(splay_cmp_fn cmp) {
+    splay_tree *tree;
+
+    if((tree = malloc(sizeof(splay_tree))) == NULL)
+        return NULL;
+
+    tree->cmp = cmp;
+    tree->root = NULL;
+
+    return tree;
+}
 
 void splay_tree_destroy(splay_tree *tree) {}
 
